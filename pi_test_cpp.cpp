6e859a66d4bd52f661f28a942cdb2af19db2d298
tennisbowling <enzovanthorre@hotmail.com>
2021-05-14 11:34:00 -0500

    clean up cpp

--- pi_test_cpp.cpp
+++ pi_test_cpp.cpp
@@ -1,41 +1,19 @@
-#include <chrono>
+#include <ctime>
 #include <iostream>
 using namespace std;
-class Timer
-{
-private:
-	using clock_t = std::chrono::high_resolution_clock;
-	using second_t = std::chrono::duration<double, std::ratio<1> >;
-	
-	std::chrono::time_point<clock_t> m_beg;
- 
-public:
-	Timer() : m_beg(clock_t::now())
-	{
-	}
-	
-	void reset()
-	{
-		m_beg = clock_t::now();
-	}
-	
-	double elapsed() const
-	{
-		return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
-	}
-};
 void fib(){
-        float pi {0};
-        float n = {4};
-        float d = {1};
-        float a;
-        Timer t;
+        double pi {0};
+        double n {4};
+        double d {1};
+        double a;
+        clock_t start = clock();
         for (int i = 1; i < 100000000; i++){
          a=2*(i%2)-1;
          pi+=a*n/d;
          d+=2;
         }
-		float time_elapsed = t.elapsed();
+		clock_t finish = clock();
+        double time_elapsed = (finish - start) / static_cast<double>( CLOCKS_PER_SEC );
         std::cout << "c++'s pi value is: \n" << pi << "\n c++'s time elapsed is: \n" << time_elapsed;
 };
 int main() {
